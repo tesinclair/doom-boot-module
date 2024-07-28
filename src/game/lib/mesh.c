c151fff78212f5fc286a76f03e541a31d161978e
Tai Sinclair <taisinclair4@gmail.com>
2024-07-28 21:06:07 +0100

    holds off adding textures until after basic colors are implemented

--- src/game/lib/mesh.c
+++ src/game/lib/mesh.c
@@ -1,1 +1,30 @@
 #include "mesh.h"
+
+int
+draw_mesh_box(
+        void *double_buf,
+        size_t x,
+        size_t y,
+        size_t z,
+        size_t height,
+        size_t width,
+        size_t depth,
+        size_t color
+        ){
+    return -1;
+}
+
+int
+draw_mesh_bubble(
+        void *double_buf,
+        size_t x,
+        size_t y,
+        size_t z,
+        size_t radius_x,
+        size_t radius_y,
+        size_t radius_z,
+        size_t color
+        ){
+    return -1;
+}
+
